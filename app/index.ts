45e9664cd85ca36199994cf0b46376379c98d416
Gary Boyle <gary@shayoo.ie>
2021-03-24 17:09:23 +0000

    Add boostrap sass

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,4 @@
-// Import bootstrap
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/main.scss";
 
 import Customer from "./customer";
 
--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,26 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: "style-loader", // inject CSS to page
+          },
+          {
+            loader: "css-loader", // translates CSS into CommonJS modules
+          },
+          {
+            loader: "postcss-loader", // Run post css actions
+            options: {
+              postcssOptions: {
+                plugins: ["precss", "autoprefixer"],
+              },
+            },
+          },
+          {
+            loader: "sass-loader", // compiles Sass to CSS
+          },
+        ],
       },
     ],
   },
