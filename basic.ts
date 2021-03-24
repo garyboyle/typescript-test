492e59e0b96f7ba1a6498ea93c6d238101acb87e
Gary Boyle <gary@shayoo.ie>
2021-03-24 12:30:35 +0000

    Save the root dir

--- basic.ts
+++
@@ -1,9 +0,0 @@
-import Customer from "./customer";
-
-let firstCustomer = new Customer("Alice");
-
-let announcement: string = firstCustomer.announce();
-
-let heading = document.querySelector("h1");
-
-heading!.textContent = announcement;
